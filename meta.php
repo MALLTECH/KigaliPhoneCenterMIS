da16aeeb0ba0049b024cbbedd0a182dcc68dd5f7
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-10-04 15:11:55 +0200

    merge conflict

--- meta.php
+++ meta.php
@@ -1,5 +1,5 @@
 <?php
-$isDarkMode=true;
+$isDarkMode=false;
 if ($isDarkMode):
 ?>
 <link rel="stylesheet" type="text/css" href="assets/css/themeDark.css">
@@ -7,4 +7,3 @@ if ($isDarkMode):
 <link rel="stylesheet" type="text/css" href="assets/css/themeLight.css">
 <?php endif?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
